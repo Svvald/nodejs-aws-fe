5102e6795c892237f79165fdaf6926250713dd52
Nikita Chistousov <nc@svvald.me>
2020-11-15 19:38:33 +0300

    feat: integrate import products file with lambda

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://duahs76lv2.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://pc711ez706.execute-api.eu-west-1.amazonaws.com/dev/',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
