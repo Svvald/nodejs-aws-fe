be96969c8234f752d5e4a298b6fd6418bb70155d
Nikita Chistousov <nc@svvald.me>
2020-12-14 01:58:47 +0300

    feat: add cart api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://pc711ez706.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://f6y53dhmo0.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
